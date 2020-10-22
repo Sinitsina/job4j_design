3618f028b63ca86058c02aa1daf91d51311e719e
Sinitsina <65966649+Sinitsina@users.noreply.github.com>
2020-10-22 14:01:37 +0500

    2. Без переопределения equals и hashCode [#362007]

---
+++ chapter_001/src/main/java/ru/job4j/hash/Store.java
@@ -0,0 +1,17 @@
+package ru.job4j.hash;
+
+import java.util.*;
+import java.util.Map;
+
+public class Store {
+    public static void main(String[] args) {
+        User u1 = new User("Ivan", 5, new GregorianCalendar(1990, 01, 01));
+        User u2 = new User("Ivan", 5, new GregorianCalendar(1990, 01, 01));
+        Map<User, Object> map = new HashMap<>();
+        map.put(u1, new Object());
+        map.put(u2, new Object());
+
+        map.entrySet().stream().forEach(e-> System.out.println(e));
+    }
+
+}
