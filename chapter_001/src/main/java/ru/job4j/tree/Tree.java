bb44f078addebbdc331cc9fc74e7f028d8692089
Sinitsina <65966649+Sinitsina@users.noreply.github.com>
2020-11-01 21:17:57 +0500

    1. Создать элементарную структуру дерева [#361992]

--- chapter_001/src/main/java/ru/job4j/tree/Tree.java
+++ chapter_001/src/main/java/ru/job4j/tree/Tree.java
@@ -1,5 +1,6 @@
 package ru.job4j.tree;
 import java.util.*;
+import java.util.function.Predicate;
 
 public class Tree<E> implements SimpleTree<E> {
     private final Node<E> root;
@@ -11,42 +12,41 @@ public class Tree<E> implements SimpleTree<E> {
     @Override
     public boolean add(E parent, E child) {
         boolean rsl = false;
-        if (findBy(parent).isPresent() && !findBy(child).isPresent()) {
-            Node<E> parentNode = findBy(parent).get();
+        Optional<Node<E>> el = findBy(parent);
+        if (el.isPresent() && !findBy(child).isPresent()) {
+            Node<E> parentNode = el.get();
             Node<E> childNode = new Node<>(child);
             parentNode.getChildren().add(childNode);
             rsl = true;
         }
-
         return rsl;
     }
 
     @Override
     public Optional<Node<E>> findBy(E value) {
+        Predicate<Node<E>> predicate = x -> x.getValue().equals(value);
+        Optional<Node<E>> rsl = findByPredicate(predicate);
+        return rsl;
+    }
+
+    public boolean isBinary() {
+        Predicate<Node<E>> predicate = x -> x.getChildren().size() > 2;
+        Optional<Node<E>> rsl = findByPredicate(predicate);
+        return rsl.isEmpty();
+    }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> predicate) {
         Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.getValue().equals(value)) {
+            if (predicate.test(el)) {
                 rsl = Optional.of(el);
                 break;
             }
             data.addAll(el.getChildren());
         }
         return rsl;
-    }
-
-    public boolean isBinary() {
-        Queue<Node<E>> data = new LinkedList<>();
-        data.offer(this.root);
-        while (!data.isEmpty()) {
-            Node<E> el = data.poll();
-            if (el.getChildren().size() > 2) {
-                return false;
-            }
-            data.addAll(el.getChildren());
-        }
-        return true;
     }
 }
