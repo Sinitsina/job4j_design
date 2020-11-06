e6691879e7335cd4205c57827067fe0bad901394
Sinitsina <65966649+Sinitsina@users.noreply.github.com>
2020-11-06 10:56:10 +0500

    1. Читаем файл конфигурации [#362029]

---
+++ chapter_002/src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,49 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.StringJoiner;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                if (line.isEmpty() || !line.contains("=")) {
+                    continue;
+                }
+                String[] words = line.split("=");
+                values.put(words[0], words[1]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("./app.properties"));
+    }
+}
