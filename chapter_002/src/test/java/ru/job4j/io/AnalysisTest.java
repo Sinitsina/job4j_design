237680e26ecd17316ad2597e8173959cd42801a9
Sinitsina <65966649+Sinitsina@users.noreply.github.com>
2020-11-11 17:23:27 +0500

    2. Анализ доступности сервера. [#362030]

--- chapter_002/src/test/java/ru/job4j/io/AnalysisTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalysisTest.java
@@ -2,6 +2,12 @@ package ru.job4j.io;
 
 import org.junit.Test;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class AnalysisTest {
 
     @Test
@@ -10,6 +16,8 @@ public class AnalysisTest {
         String target = "result.txt";
         Analysis test = new Analysis();
         test.unavailable(source, target);
+        assertThat(Files.exists(Path.of("/Users/olga/IdeaProjects/job4j_design/chapter_002/result.txt")),
+                is(true));
     }
 
 }
