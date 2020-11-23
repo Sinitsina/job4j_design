aa2c939e0ce72f8c93fde25a443d75a73bb4bee6
Sinitsina <65966649+Sinitsina@users.noreply.github.com>
2020-11-23 11:59:26 +0500

    5. Валидация параметров запуска. [#362022]

--- chapter_002/src/main/java/ru/job4j/io/Dir.java
+++ chapter_002/src/main/java/ru/job4j/io/Dir.java
@@ -4,11 +4,14 @@ import java.io.File;
 
 public class Dir {
     public static void main(String[] args) {
-        if (args.length == 0) {
+        if (args.length <= 0) {
             throw new IllegalArgumentException(
                     "Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
         }
-        File file = new File(args[0]);
+        File file = new File("");
+        if (args.length > 0) {
+            file = new File(args[0]);
+        }
         if (!file.exists()) {
             throw new IllegalArgumentException(
                     String.format("Not exist %s", file.getAbsoluteFile()));
