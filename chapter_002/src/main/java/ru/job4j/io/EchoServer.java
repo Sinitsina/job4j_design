34543335126acc7cfa4adad46067048431ea7412
Sinitsina <65966649+Sinitsina@users.noreply.github.com>
2020-12-02 12:24:31 +0500

    1. Бот [#362046]

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -7,7 +7,8 @@ import java.net.Socket;
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
-            while (true) {
+            boolean close = true;
+            while (close) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
@@ -15,8 +16,22 @@ public class EchoServer {
                     String str;
                     while (!(str = in.readLine()).isEmpty()) {
                         System.out.println(str);
+                        if (str.contains("msg=Hello")) {
+                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                            out.write("Hello, dear friend.".getBytes());
+                            break;
+
+                        } else if (str.contains("msg=Exit")) {
+                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                            out.write("Closed.".getBytes());
+                            close = false;
+                            break;
+                        } else if (!str.contains("Hello") && !str.contains("msg=Exit")){
+                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                            out.write("What.\r\n\r\n".getBytes());
+                            break;
+                        }
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                 }
             }
         }
