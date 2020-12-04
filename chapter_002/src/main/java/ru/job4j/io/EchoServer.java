98846bd7d0bba4dcd3b9aa51a30d37cf0c1f945e
Sinitsina <65966649+Sinitsina@users.noreply.github.com>
2020-12-04 16:23:02 +0500

    1. Бот [#362046]

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -3,37 +3,43 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class EchoServer {
+    public static boolean close = true;
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
-            boolean close = true;
+
             while (close) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str;
-                    while (!(str = in.readLine()).isEmpty()) {
-                        System.out.println(str);
-                        if (str.contains("msg=Hello")) {
-                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                            out.write("Hello, dear friend.".getBytes());
-                            break;
+                    String res = "";
+                    StringBuilder sb = new StringBuilder();
 
-                        } else if (str.contains("msg=Exit")) {
-                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                            out.write("Closed.".getBytes());
-                            close = false;
-                            break;
-                        } else if (!str.contains("Hello") && !str.contains("msg=Exit")){
-                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                            out.write("What.\r\n\r\n".getBytes());
-                            break;
-                        }
+                    while (!(str = in.readLine()).isEmpty()) {
+                       sb.append(str);
                     }
+
+                    final String line = sb.toString();
+
+                    if (line.contains("msg=Hello")) {
+                        res = "Hello, dear friend.";
+                    } else if (line.contains("msg=Exit")) {
+                        res = "Closed.";
+                        close = false;
+                    } else if (!line.contains("Hello") && !line.contains("msg=Exit")){
+                        res = "What.";
+                    }
+
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(res.getBytes(StandardCharsets.UTF_8));
                 }
             }
         }
+        }
     }
-}
+
--- chapter_002/src/main/java/ru/job4j/io/Zip.java
+++ chapter_002/src/main/java/ru/job4j/io/Zip.java
@@ -8,11 +8,13 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-    public void packFiles(List<File> sources, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            for(File el : sources) {
-                zip.putNextEntry(new ZipEntry(el.getPath()));
-                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(el))) {
+    public void packFiles(List<Path> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(
+                new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (Path el : sources) {
+                zip.putNextEntry(new ZipEntry(String.valueOf(el)));
+                try (BufferedInputStream out = new BufferedInputStream(
+                        new FileInputStream(String.valueOf(el)))) {
                     zip.write(out.readAllBytes());
                 }
             }
@@ -22,7 +24,8 @@ public class Zip {
     }
 
     public void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+        try (ZipOutputStream zip = new ZipOutputStream(
+                new BufferedOutputStream(new FileOutputStream(target)))) {
             zip.putNextEntry(new ZipEntry(source.getPath()));
             try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
                 zip.write(out.readAllBytes());
@@ -32,6 +35,33 @@ public class Zip {
         }
     }
 
+    public static List<Path> searchForException(List<Path> sources, String s) throws IOException {
+        SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(s));
+        for (Path file : sources) {
+            Files.walkFileTree(file, searcher);
+        }
+        return searcher.getPaths();
+
+        //2. Для поиска используйте класс из прошлого задания. Поиск вынесите в отдельный метод
+
+        //return null;
+    }
+
+    //public static void search(final String pattern, final File folder, List<String> result) {
+    //        for (final File f : folder.listFiles()) {
+    //
+    //            if (f.isDirectory()) {
+    //                search(pattern, f, result);
+    //            }
+    //
+    //            if (f.isFile()) {
+    //                if (f.getName().matches(pattern)) {
+    //                    result.add(f.getAbsolutePath());
+    //                }
+    //            }
+    //
+    //        }
+
     public static void main(String[] args) throws IOException {
         new Zip().packSingleFile(
                 new File("./chapter_005/pom.xml"),
@@ -39,12 +69,23 @@ public class Zip {
         );
 
         ArgZip arg = new ArgZip(args);
-        List<File> sources = Files.walk(Paths.get(arg.directory()))
-                .filter(Files::isRegularFile)
-                .filter(p -> !p.toFile().getName().endsWith(arg.exclude()))
-                .map(Path::toFile)
-                .collect(Collectors.toList());
 
-        new Zip().packFiles(sources, new File(arg.output()));
+        /*List<Path> sources = Files.walk(Paths.get(arg.directory()))
+                .filter(Files::isRegularFile)
+                //.filter(p -> !p.toFile().getName().endsWith(arg.exclude()))
+                .map(Path::toFile)
+                .collect(Collectors.toList());*/
+
+        List<Path> sources = Files.list(Paths.get(arg.directory()))
+                .filter(Files::isRegularFile)
+                //.map(path -> path.resolve("..."))
+                .collect(Collectors.toList());
+        //searchForException(sources, arg.exclude());
+
+        //File fileRoot = new File(arg.directory());
+
+        //List<File> s = Arrays.asList(fileRoot.listFiles());
+
+        new Zip().packFiles(searchForException(sources, arg.exclude()), new File(arg.output()));
     }
 }
