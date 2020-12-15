10da68619e563640f5be68ec750e05e98a0f132c
Sinitsina <65966649+Sinitsina@users.noreply.github.com>
2020-12-15 16:17:29 +0500

    0. Видео [#361955]

---
+++ chapter_003/src/main/java/Test.sql
@@ -0,0 +1,16 @@
+create database hotel;
+create table rooms (
+                       id serial primary key,
+                       room_Number int,
+                       vacant bool,
+                       type text
+);
+insert into rooms(room_Number, vacant, type) values('135', 'true', 'standard');
+select *
+from rooms;
+update rooms set vacant = 'false';
+select *
+from rooms;
+delete from rooms;
+select *
+from rooms;
