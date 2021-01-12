6f6e3d9cfb3ec323d9961c26abe87d38fe5dac36
Sinitsina <65966649+Sinitsina@users.noreply.github.com>
2021-01-12 11:16:44 +0500

    1. Схема прав пользователи и роли. [#361954]

--- chapter_003/src/main/java/create.sql
+++ chapter_003/src/main/java/create.sql
@@ -1,51 +1,49 @@
-create table users(
-    id serial primary key,
-    name varchar(50),
-    role_id int references role(id)
-);
-
 create table role(
-    id serial primary key,
-    role_name varchar(50)
+                     id serial primary key,
+                     role_name varchar(50)
 );
 
+create table users(
+                      id serial primary key,
+                      name varchar(50),
+                      role_id int references role(id)
+);
 create table rules(
-    id serial primary key,
-    rule_desc varchar(500)
-);
-
-create table item(
-    id serial primary key,
-    item_desc varchar(300),
-    user_id int references users(id),
-    comment_id int references comments(id),
-    attach_id int references attachments(id)
-);
-
-create table comments(
-    id serial primary key,
-    comment varchar(500)
-);
-
-create table attachments(
-    id serial primary key,
-    attach_name varchar(100)
+                      id serial primary key,
+                      rule_desc varchar(500)
 );
 
 create table state(
-    id serial primary key,
-    state_desc text,
-    item_id int references item(id)
+                      id serial primary key,
+                      state_desc text
 );
 
 create table category(
-    id serial primary key,
-    category_name varchar(50),
-    item_id int references item(id)
+                         id serial primary key,
+                         category_name varchar(50)
+);
+create table item(
+                     id serial primary key,
+                     item_desc varchar(300),
+                     user_id int references users(id),
+                     state_id int references state(id),
+                     category_id int references category(id)
+
+);
+create table comments(
+                         id serial primary key,
+                         comment varchar(500),
+                         item_id int references item(id)
+);
+
+create table attachments(
+                            id serial primary key,
+                            attach_name varchar(100),
+                            item_id int references item(id)
 );
 
 create table role_rules(
-    id serial primary key,
-    role_id int references role(id),
-    rules_id int references rules(id)
+                           id serial primary key,
+                           role_id int references role(id),
+                           rules_id int references rules(id)
 );
--- chapter_003/src/main/java/insert.sql
+++ chapter_003/src/main/java/insert.sql
@@ -1,9 +1,9 @@
-insert into users(name, role_id) values('Ivan', '1');
 insert into role(role_name) values('private buyer');
+insert into users(name, role_id) values('Ivan', '1');
 insert into rules(rule_desc) values('prepayment');
-insert into item(item_desc, user_id, comment_id, attach_id) values('purchase meat', '1', '1', '1');
-insert into comments(comment) values('delivery needed');
-insert into attachments(attach_name) values ('payment order');
-insert into state(state_desc, item_id) values ('in progress', '1');
-insert into category(category_name, item_id) values ('purchase with delivery', '1');
+insert into state(state_desc) values ('in progress');
+insert into category(category_name) values ('purchase with delivery');
+insert into item(item_desc, user_id, state_id, category_id) values('purchase meat', '1', '1', '1');
+insert into comments(comment, item_id) values('delivery needed', '1');
+insert into attachments(attach_name, item_id) values ('payment order', '1');
 insert into role_rules(role_id, rules_id) values ('1', '1');
